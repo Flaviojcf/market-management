d02a859c16093331959a279a169b8e8acdaaf13c
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-14 10:34:18 -0300

    feat: Create baseEntity

---
+++ MarketManagement.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace MarketManagement.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity() { }
+
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; private set; }
+        public bool IsActive { get; private set; } = true;
+
+
+        public void Activate()
+        {
+            this.IsActive = true;
+            this.UpdatedAt = DateTime.Now;
+        }
+
+        public void Deactivate()
+        {
+            this.IsActive = false;
+            this.UpdatedAt = DateTime.Now;
+        }
+    }
+}
