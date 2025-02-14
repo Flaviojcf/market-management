717b947b781000cd9c14e14bce8759b5c3abd7da
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-14 14:58:40 -0300

    feat: Add user entity

---
+++ MarketManagement.Domain/Constants/DomainMessageConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketManagement.Domain.Constants
+{
+    public static class DomainMessageConstant
+    {
+        public const string messageFieldIsRequired = "The field {0} is required";
+        public const string messageFieldIsRequiredAndGreaterThan = "The field {0} is required and must be greater than {1}";
+    }
+}
---
+++ MarketManagement.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketManagement.Domain.Exceptions
+{
+    public class DomainException(string error) : Exception(error)
+    {
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainException(error);
+            }
+        }
+    }
+}
