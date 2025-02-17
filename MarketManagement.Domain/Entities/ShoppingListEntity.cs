68198fc1f7d68cd286954be83b084c72bfa80245
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-17 16:47:41 -0300

    feat: Add shoping list

---
+++ MarketManagement.Domain/Entities/ShoppingListEntity.cs
@@ -0,0 +1,38 @@
+﻿using MarketManagement.Domain.Constants;
+using MarketManagement.Domain.Exceptions;
+
+namespace MarketManagement.Domain.Entities
+{
+    public class ShoppingListEntity : BaseEntity
+    {
+        public ShoppingListEntity(string name, int totalPrice, Guid productId)
+        {
+            ValidateDomain(name, totalPrice, productId);
+            Name = name;
+            TotalPrice = totalPrice;
+            ProductId = productId;
+        }
+
+        public string Name { get; private set; }
+        public int TotalPrice { get; private set; }
+
+        public Guid ProductId { get; private set; }
+        public ProductEntity? Product { get; private set; }
+
+        public void Update(string name, int totalPrice, Guid productId)
+        {
+            ValidateDomain(name, totalPrice, productId);
+            Name = name;
+            TotalPrice = totalPrice;
+            ProductId = productId;
+            UpdatedAt = DateTime.Now;
+        }
+
+        private static void ValidateDomain(string name, int totalPrice, Guid productId)
+        {
+            DomainException.When(string.IsNullOrEmpty(name), string.Format(DomainMessageConstant.messageFieldIsRequired, "name"));
+            DomainException.When(totalPrice == 0, string.Format(DomainMessageConstant.messageFieldIsRequiredAndGreaterThan, "totalPrice", 0));
+            DomainException.When(productId == Guid.Empty, string.Format(DomainMessageConstant.messageFieldIsRequired, "productId"));
+        }
+    }
+}
---
+++ MarketManagement.Tests/Domain/ShoppingListUnitTest.cs
@@ -0,0 +1,122 @@
+﻿using MarketManagement.Domain.Entities;
+using MarketManagement.Domain.Exceptions;
+
+namespace MarketManagement.Tests.Domain
+{
+    [Collection(nameof(ShoppingListEntity))]
+    public class ShoppingListUnitTest
+    {
+        [Fact]
+        [Trait("Domain", "Create Shopping List")]
+        public void CreateShoppingList_WithValidData_ReturnsShoppingList()
+        {
+            // Arrange
+            var name = "Feira janeiro";
+            var totalPrice = 10;
+            var productId = Guid.NewGuid();
+
+            // Act
+            var shoppingList = new ShoppingListEntity(name, totalPrice, productId);
+
+            // Assert
+            Assert.NotNull(shoppingList);
+            Assert.Equal(Guid.Empty, shoppingList.Id);
+            Assert.Equal(name, shoppingList.Name);
+            Assert.Equal(totalPrice, shoppingList.TotalPrice);
+            Assert.Equal(productId, shoppingList.ProductId);
+            Assert.True(shoppingList.IsActive);
+            Assert.Equal(DateTime.Now.Date, shoppingList.CreatedAt.Date);
+            Assert.Equal(DateTime.MinValue, shoppingList.UpdatedAt.Date);
+        }
+
+        [Theory]
+        [InlineData("", 10, "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
+        [InlineData("Feira janeiro", null, "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
+        [InlineData("Feira janeiro", 10, null)]
+        [Trait("Domain", "Create Shopping List")]
+        public void CreateShoppingList_ShouldThrowDomainException_WhenInvalidDataIsProvided(string name, int totalPrice, string productId)
+        {
+            // Act & Assert
+            Assert.Throws<DomainException>(() => new ShoppingListEntity(name, totalPrice, string.IsNullOrWhiteSpace(productId) ? default : Guid.Parse(productId)));
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Shopping List")]
+        public void DeActiveShoppingList_ReturnsDeActiveShoppingList()
+        {
+            // Arrange
+            var name = "Feira janeiro";
+            var totalPrice = 10;
+            var productId = Guid.NewGuid();
+
+            // Act
+            var shoppingList = new ShoppingListEntity(name, totalPrice, productId);
+
+            shoppingList.Deactivate();
+
+            // Assert
+            Assert.NotNull(shoppingList);
+            Assert.False(shoppingList.IsActive);
+            Assert.Equal(DateTime.Now.Date, shoppingList.UpdatedAt.Date);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Shopping List")]
+        public void ActiveShoppingList_ReturnsActiveShoppingList()
+        {
+            // Arrange
+            var name = "Feira janeiro";
+            var totalPrice = 10;
+            var productId = Guid.NewGuid();
+
+            // Act
+            var shoppingList = new ShoppingListEntity(name, totalPrice, productId);
+            shoppingList.Deactivate();
+            shoppingList.Activate();
+
+            // Assert
+            Assert.NotNull(shoppingList);
+            Assert.True(shoppingList.IsActive);
+            Assert.Equal(DateTime.Now.Date, shoppingList.UpdatedAt.Date);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Shopping List")]
+        public void UpdateShoppingList_WithValidData_ReturnsShoppingList()
+        {
+            // Arrange
+            var name = "Feira janeiro";
+            var totalPrice = 10;
+            var productId = Guid.NewGuid();
+
+            // Act
+            var shoppingList = new ShoppingListEntity(name, totalPrice, productId);
+
+            shoppingList.Update("Feira fevereiro", 20, productId);
+
+            // Assert
+            Assert.NotNull(shoppingList);
+            Assert.Equal(Guid.Empty, shoppingList.Id);
+            Assert.Equal("Feira fevereiro", shoppingList.Name);
+            Assert.Equal(20, shoppingList.TotalPrice);
+            Assert.Equal(productId, shoppingList.ProductId);
+            Assert.True(shoppingList.IsActive);
+            Assert.Equal(DateTime.Now.Date, shoppingList.CreatedAt.Date);
+            Assert.Equal(DateTime.Now.Date, shoppingList.UpdatedAt.Date);
+        }
+
+        [Theory]
+        [InlineData("", 10, "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
+        [InlineData("Feira janeiro", null, "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
+        [InlineData("Feira janeiro", 10, null)]
+        [Trait("Domain", "Update Shopping List")]
+        public void UpdateShoppingList_ShouldThrowDomainException_WhenInvalidDataIsProvided(string name, int totalPrice, string productId)
+        {
+            // Arrange
+            var shoppingListEntity = new ShoppingListEntity("Feira janeiro", 20, Guid.NewGuid());
+
+            // Act & Assert
+            Assert.Throws<DomainException>(() => shoppingListEntity.Update(name, totalPrice, string.IsNullOrWhiteSpace(productId) ? default : Guid.Parse(productId)));
+        }
+    }
+}
