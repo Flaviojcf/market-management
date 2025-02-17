2da71990206d21372e5c28554d014a9a6a9176af
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-17 16:34:43 -0300

    test: Create update test for price history entity

--- MarketManagement.Tests/Domain/PriceHistoryUnitTest.cs
+++ MarketManagement.Tests/Domain/PriceHistoryUnitTest.cs
@@ -79,6 +79,31 @@ namespace MarketManagement.Tests.Domain
             Assert.Equal(DateTime.Now.Date, priceHistory.UpdatedAt.Date);
         }
 
+        [Fact]
+        [Trait("Domain", "Update Price History")]
+        public void UpdatePriceHistory_WithValidData_ReturnsPriceHistory()
+        {
+            // Arrange
+            var price = 10;
+            var date = DateTime.Now;
+            var productId = Guid.NewGuid();
+
+            // Act
+            var priceHistory = new PriceHistoryEntity(price, date, productId);
+
+            priceHistory.Update(20, date, productId);
+
+            // Assert
+            Assert.NotNull(priceHistory);
+            Assert.Equal(Guid.Empty, priceHistory.Id);
+            Assert.Equal(20, priceHistory.Price);
+            Assert.Equal(date, priceHistory.Date);
+            Assert.Equal(productId, priceHistory.ProductId);
+            Assert.True(priceHistory.IsActive);
+            Assert.Equal(DateTime.Now.Date, priceHistory.CreatedAt.Date);
+            Assert.Equal(DateTime.Now.Date, priceHistory.UpdatedAt.Date);
+        }
+
         [Theory]
         [InlineData(0, "2024-01-01", "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
         [InlineData(10, null, "d3b07384-d9a1-4a2d-bf43-bb6e5b7f5c01")]
