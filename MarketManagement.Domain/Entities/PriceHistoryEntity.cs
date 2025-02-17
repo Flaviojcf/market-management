f16c4a78042f32b4b1533e36f5c7278377414b49
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-17 16:26:31 -0300

    feat: Create price history unit test

---
+++ MarketManagement.Domain/Entities/PriceHistoryEntity.cs
@@ -0,0 +1,39 @@
+﻿using MarketManagement.Domain.Constants;
+using MarketManagement.Domain.Exceptions;
+
+namespace MarketManagement.Domain.Entities
+{
+    public class PriceHistoryEntity : BaseEntity
+    {
+        public PriceHistoryEntity(int price, DateTime date, Guid productId)
+        {
+            ValidateDomain(price, date, productId);
+            Price = price;
+            Date = date;
+            ProductId = productId;
+        }
+
+        public int Price { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public Guid ProductId { get; private set; }
+        public ProductEntity? Product { get; private set; }
+
+
+        public void Update(int price, DateTime date, Guid productId)
+        {
+            ValidateDomain(price, date, productId);
+            Price = price;
+            Date = date;
+            ProductId = productId;
+            UpdatedAt = DateTime.Now;
+        }
+
+        private static void ValidateDomain(int price, DateTime date, Guid productId)
+        {
+            DomainException.When(price == 0, string.Format(DomainMessageConstant.messageFieldIsRequiredAndGreaterThan, "price", 0));
+            DomainException.When(date == DateTime.MinValue, string.Format(DomainMessageConstant.messageFieldIsRequired, "date"));
+            DomainException.When(productId == Guid.Empty, string.Format(DomainMessageConstant.messageFieldIsRequired, "productId"));
+        }
+    }
+}
