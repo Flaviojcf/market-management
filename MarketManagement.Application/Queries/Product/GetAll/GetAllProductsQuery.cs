6261c8eb54dc13d1e3ce34c793eeec7ff2ae20a3
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-17 21:23:15 -0300

    feat: Add product query and tests

---
+++ MarketManagement.Application/Queries/Product/GetAll/GetAllProductsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MarketManagement.Domain.Entities;
+using MediatR;
+
+namespace MarketManagement.Application.Queries.Product.GetAll
+{
+    public class GetAllProductsQuery : IRequest<IEnumerable<ProductEntity>>
+    {
+        public GetAllProductsQuery(string query)
+        {
+            Query = query;
+        }
+
+        public string Query { get; set; }
+    }
+}
