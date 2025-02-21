8a2de69dd6c7d56baa67b37c09dba8f49e2bc148
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-21 12:55:07 -0300

    feat: Add user tests

--- MarketManagement.Tests/Application/User/CreateUserCommandHandlerTest.cs
+++ MarketManagement.Tests/Application/User/CreateUserCommandHandlerTest.cs
@@ -1,6 +1,9 @@
 ﻿using MarketManagement.Application.Commands.User.CreateUser;
 using MarketManagement.Domain.Entities;
+using MarketManagement.Domain.Exceptions;
 using MarketManagement.Domain.Repositories;
+using MarketManagement.Domain.Services.Interfaces;
+using MarketManagement.Domain.Validations;
 using Moq;
 
 namespace MarketManagement.Tests.Application.User
@@ -10,13 +13,14 @@ namespace MarketManagement.Tests.Application.User
     {
 
         private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IUserValidateService> _userValidateServiceMock;
         private readonly CreateUserCommandHandler _createUserCommandHandler;
-
 
         public CreateUserCommandHandlerTest()
         {
             _userRepositoryMock = new Mock<IUserRepository>();
-            _createUserCommandHandler = new CreateUserCommandHandler(_userRepositoryMock.Object);
+            _userValidateServiceMock = new Mock<IUserValidateService>();
+            _createUserCommandHandler = new CreateUserCommandHandler(_userRepositoryMock.Object, _userValidateServiceMock.Object);
         }
 
 
@@ -30,6 +34,9 @@ namespace MarketManagement.Tests.Application.User
             var password = "password";
             var monthlyBudget = 1200;
             var command = new CreateUserCommand(name, email, password, monthlyBudget);
+            var validationResult = new ValidationResult();
+
+            _userValidateServiceMock.Setup(r => r.ValidateCreateUserAsync(email)).ReturnsAsync(validationResult);
 
             _userRepositoryMock.Setup(r => r.CreateAsync(It.IsAny<UserEntity>())).Returns(Task.CompletedTask);
 
@@ -37,8 +44,34 @@ namespace MarketManagement.Tests.Application.User
             var result = await _createUserCommandHandler.Handle(command, CancellationToken.None);
 
             // Assert
+            Assert.True(validationResult.IsValid);
             Assert.Equal(Guid.Empty, result);
             _userRepositoryMock.Verify(r => r.CreateAsync(It.IsAny<UserEntity>()), Times.Once);
+        }
+
+        [Fact]
+        [Trait("Application", "Create User - Command")]
+        public async Task ShouldNot_CreateUser_WhenEmailIsAlreadyRegistered()
+        {
+            // Arrange
+            var name = "John Doe";
+            var email = "johnDoe@gmail.com";
+            var password = "password";
+            var monthlyBudget = 1200;
+            var command = new CreateUserCommand(name, email, password, monthlyBudget);
+            var validationResult = new ValidationResult();
+
+            validationResult.AddError($"Email '{email}' já cadastrado.");
+
+            _userValidateServiceMock.Setup(r => r.ValidateCreateUserAsync(email)).ReturnsAsync(validationResult);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<ValidationException>(() => _createUserCommandHandler.Handle(command, CancellationToken.None));
+
+            // Assert
+            Assert.Equal($"Email '{email}' já cadastrado.", exception.Message);
+            Assert.False(validationResult.IsValid);
+            _userRepositoryMock.Verify(r => r.CreateAsync(It.IsAny<UserEntity>()), Times.Never);
         }
     }
 }
